450e0ddc5623bf25b21b1f8898bdbf7308d3eb3d
Federico Gomez <gomez00federico@gmail.com>
2022-12-12 01:43:14 -0300

    navbar and mobile_menu merged

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const navbar_email = document.querySelector('.navbar-email');
 const desktop_menu = document.querySelector('.desktop-menu');
 
+const icon_menu = document.querySelector('.menu');
+const mobile_menu = document.querySelector('.mobile-menu');
+
 navbar_email.addEventListener('click', toggleDesktopMenu);
+icon_menu.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu() {
     /**
+     * Muestra el menu en desktop -> cuando se da click al email (desktop_menu)
      * La función agrega o quita (dependiendo si ya está o no) la clase inactive al desktop menu
     */
     
@@ -19,4 +24,13 @@ function toggleDesktopMenu() {
     // }
 
     desktop_menu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu() {
+    /**
+     * Muestra el menu en mobile -> cuando se da click en el icono de menu (icon_menu)
+     * La función agrega o quita (dependiendo si ya está o no) la clase inactive al mobile menu
+    */
+
+    mobile_menu.classList.toggle('inactive');
 }
